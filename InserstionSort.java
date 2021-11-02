838fddc41b97b82940c659f60abd95ac13803624
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-02 17:12:28 +0530

    Insertion Sort

---
+++ InserstionSort.java
@@ -0,0 +1,25 @@
+package com.dsandalgorithm;
+
+public class InserstionSort {
+    public static void main(String[] args) {
+        String[] arr = { "ruturaj", "rohit", "lokesh", "sachin", "virat" };
+        String sortedArray[] = sort(arr, arr.length);
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+    }
+
+    public static String[] sort(String array[], int length) {
+        String temp = "";
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
